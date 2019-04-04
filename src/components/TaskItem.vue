060049d5a10adf5e0c279ed85506621a5ab7bca5
Vladimir Abramyants <vabramanc@gmaill.com>
2019-04-04 17:07:06 +0300

    Create new component: TaskItem

---
+++ src/components/TaskItem.vue
@@ -0,0 +1,26 @@
+<template>
+    <tr>
+        <th scope="row">
+            <input type="checkbox" name="coplete-field" class="check-completed" @click="$emit('completed', todo.id)">
+        </th>
+        <th scope="row" class="col-9">
+            <router-link to="/:groupName/:taskName/edit">{{ todo.title }}</router-link>
+        </th>
+        <th scope="row" class="col-2">
+            <button class="btn btn-danger" @click="$emit('remove', todo.id)">X</button>
+        </th>
+    </tr>
+</template>
+
+<script>
+export default {
+    
+    props: {
+        todo: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+
