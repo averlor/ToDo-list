1bbf846ff0d4486d2d3787f97f3d049148a05d21
Vladimir Abramyants <vabramanc@gmaill.com>
2019-04-04 15:03:11 +0300

    Added new link

--- src/router.js
+++ src/router.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
 import Home from './views/Home.vue'
+import GroupList from './views/GroupList.vue'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
-  base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
@@ -20,6 +21,11 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/grouplist',
+      name: 'grouplist',
+      component: GroupList
     }
   ]
 })
