43f5d7d61d131a87272f0d7dc2076dc6e00e7c7a
Vladimir Abramyants <vabramanc@gmaill.com>
2019-04-04 17:08:43 +0300

    Edit view: TaskList

---
+++ src/views/TaskList.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="group-list container">
+        <section class="header">
+            <InputText v-model.trim="todoText" :addTodo="addTodo" @keydown.enter.prevent="addTodo"/>
+        </section>
+        <section class="content">
+            <table class="table table-striped table-light" v-if="todos.length">
+                <thead>
+                    <tr>
+                        <th scope="col">
+                            Group of Tasks
+                        </th>
+                    </tr>       
+                </thead>
+                <tbody>
+                    <TaskItem v-for="todo in todos" :key="todo.id" @remove="remove(todo)"/>
+                </tbody>
+            </table>
+            <p v-else class="content__hello-message">
+                Nothing left in the list. Add a new todo in the input above.
+            </p>
+        </section>
+        <section class="footer">
+            <FooterInfo @removeAll="removeAll" v-if='todos.length'/>
+        </section>
+    </div>
+</template>
+
+<script>
+
+import InputText from '@/components/InputText.vue'
+import TaskItem from '@/components/TaskItem.vue'
+import FooterInfo from '@/components/FooterInfo.vue'
+
+export default {
+    
+    name: 'task-list',
+    components: {
+        InputText,
+        TaskItem,
+        FooterInfo
+    }
+}
+</script>
+
