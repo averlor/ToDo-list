9056e3b91f125bdc4894fb6df9d515e11bc5dda4
Vladimir Abramyants <vabramanc@gmaill.com>
2019-04-04 17:03:06 +0300

    Edit component: InputText

--- src/components/InputText.vue
+++ src/components/InputText.vue
@@ -2,10 +2,10 @@
     <div>
         <form @submit.prevent="addTodo" class="form">
             <div class="input-group mb3">
-                <input type="text" id="new-todo" v-model.trim="todoText" placeholder="Added new todo...">
-            </div>
-            <div class="input-group-append">
-                <button class="btn btn-secondary">ADD</button>
+                <input type="text" id="new-todo" class="form-control" v-on="listeners" :value="value" placeholder="Added new todo...">
+                <div class="input-group-append">
+                    <button class="btn btn-secondary" @click.prevent="handleAdd">ADD</button>
+                </div>
             </div>
         </form>
     </div>
@@ -14,9 +14,27 @@
 <script>
 export default {
     name: 'InputText',
+    props: {
+        value: {
+            type: String,
+            default: ''
+        },
+        addTodo: {
+            type: Function
+        }
+    },
     computed: {
-        todoText() {
-            return this.$store.state.nextTodoText
+        listeners() {
+            return {
+
+            ...this.$listeners,
+            input: event => this.$emit('input', event.target.value)
+            }
+        }
+    },
+    methods: {
+        handleAdd() {
+            this.AddTodo(this.value)
         }
     }
 }
