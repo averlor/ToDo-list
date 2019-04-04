f4616f6e9bc20edd742d626007d74c481a4d2abd
Vladimir Abramyants <vabramanc@gmaill.com>
2019-04-04 17:08:02 +0300

    Edit router: added new link

--- src/router.js
+++ src/router.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import Home from './views/Home.vue'
 import GroupList from './views/GroupList.vue'
+import TaskList from './views/TaskList.vue'
+import TaskEdit from './views/TaskEdit.vue'
 
 Vue.use(Router)
 
@@ -11,21 +12,18 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    },
-    {
-      path: '/grouplist',
       name: 'grouplist',
       component: GroupList
+    },
+    {
+      path: '/:groupName',
+      name: 'groupname',
+      component: TaskList
+    },
+    {
+      path: '/:groupName/:taskName/edit',
+      name: '',
+      component:TaskEdit
     }
   ]
 })
