c037f79028000521c15986573682a68b62ba1169
Vladimir Abramyants <vabramanc@gmaill.com>
2019-04-05 12:37:59 +0300

    Edit component: remove "Completed" and "Pending". Added "Count of groups"

--- src/components/FooterInfo.vue
+++ src/components/FooterInfo.vue
@@ -1,10 +1,7 @@
 <template>
     <div class="form-inline">
-        <div class="block form-group col-5">
-            <p class="block__info">Completed: {{ completed }}</p>
-        </div>
-        <div class="from-group col-5">
-            <p class="block__info">Pending: {{ pending }}</p>
+        <div class="block form-group col-10">
+            <p class="block__info">Count of groups: {{ all }}</p>
         </div>
         <div class="col-2">
             <button class="btn btn-danger" @click="$emit('removeAll')">remove all</button>
@@ -16,11 +13,8 @@
 export default {
     name: 'FooterInfo',
     computed: {
-        completed() {
-            return this.$store.getters.CompletedTodo
-        },
-        pending() {
-            return this.$store.getters.PendingTodo
+        all() {
+            return this.$store.getters.GetTodosCont
         }
     }
 }
