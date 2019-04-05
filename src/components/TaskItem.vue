a7c08a49605f590cb27fcb9c01a58670271c20ad
Vladimir Abramyants <vabramanc@gmaill.com>
2019-04-05 12:41:03 +0300

    Edit components: rewrite logic

--- src/components/TaskItem.vue
+++ src/components/TaskItem.vue
@@ -3,8 +3,14 @@
         <th scope="row">
             <input type="checkbox" name="coplete-field" class="check-completed" @click="$emit('completed', task.id)">
         </th>
-        <th scope="row" class="col-9">
-            <router-link to="/:groupName/:taskName/edit">{{ task.titleTask }}</router-link>
+        <th scope="row" class="col-9" v-if="!edit">
+            <router-link to="/:groupName/:taskName/edit">{{ title }}</router-link>
+        </th>
+        <th scope="row" class="col-9" v-else>
+            <input type="text" v-model="title">
+        </th>
+        <th scope="row" class="col-2">
+            <button class="btn btn-warning" @click="editTitle">EDIT</button>
         </th>
         <th scope="row" class="col-2">
             <button class="btn btn-danger" @click="$emit('remove', task.id)">X</button>
@@ -14,13 +20,31 @@
 
 <script>
 export default {
-    
-    props: {
-        task: {
-            type: Object,
-            required: true
-        }
+  data() {
+    return {
+      edit: false,
+      title: this.task.title
+    };
+  },
+  methods: {
+    editTitle() {
+      if (this.edit) {
+        this.saveTitle();
+      }
+      this.edit = !this.edit;
+    },
+    saveTitle() {
+      this.$store.commit("editTodo", { id: this.task.id, title: this.title });
+    },
+    getTodoTask() {
+        this.$store.commit('GetTodoTask', {id: this.task.id})
     }
-}
+  },
+  props: {
+    task: {
+      type: Object,
+      required: true
+    }
+  }
+};
 </script>
-
