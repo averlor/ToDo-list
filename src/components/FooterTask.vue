e978f76523172c59dec40ed1be3277ea17986f06
Vladimir Abramyants <vabramanc@gmaill.com>
2019-04-08 16:45:21 +0300

    Comment "Completed" and "Pending"

--- src/components/FooterTask.vue
+++ src/components/FooterTask.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="form-inline">
         <div class="block form-group col-5">
-            <p class="block__info">Completed: {{ completed }}</p>
+            <!-- <p class="block__info">Completed: {{ completed }}</p> -->
         </div>
         <div class="from-group col-5">
-            <p class="block__info">Pending: {{ pending }}</p>
+            <!-- <p class="block__info">Pending: {{ pending }}</p> -->
         </div>
         <div class="col-2">
             <button class="btn btn-danger" @click="$emit('removeAllTask')">remove all</button>
