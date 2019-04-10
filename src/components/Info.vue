1e65936b122ddba955a7edf80770bc8fde1a15fd
Vladimir Abramyants <vabramanc@gmaill.com>
2019-04-11 02:17:33 +0300

    Refactoring #18: Rename component - FooterInfo -> Info

---
+++ src/components/Info.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="form-inline">
+        <div class="block form-group col-10">
+            <p class="block__info">Count of groups: {{ all }}</p>
+        </div>
+        <div class="col-2">
+            <button class="btn btn-danger" @click="$emit('removeAll')">remove all</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Info',
+    computed: {
+        all() {
+            return this.$store.getters.GET_TODOS_COUNT
+        }
+    }
+}
+</script>
